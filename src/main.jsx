1ac49ff9d88017b7ba2c983ecc64edf2622dbb8c
Mainul Islam <mainulislam6168@gmail.com>
2024-04-22 18:05:41 +0600

    initial project setup with react-router-dom, tailwind and firebase

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import AuthProvider from './providers/AuthProvider.jsx'
+import { Toaster } from 'react-hot-toast'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/Routes.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <Toaster />
+    <RouterProvider router={router}></RouterProvider>
+  </AuthProvider>,
+)
