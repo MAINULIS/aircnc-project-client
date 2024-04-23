d0fb55e0825b7ec7b4e6278b89e3c9c53f196bdd
Mainul Islam <mainulislam6168@gmail.com>
2024-04-23 19:48:55 +0600

    home page category section and firebase authentication login, signup implemented. also implemented set profile pic from local file with imageBB

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,19 @@
+import Container from "../Shared/Container";
+import CategoryBox from "./CategoryBox";
+import { categories } from "./categoriesData";
+
+const Categories = () => {
+    return (
+        <Container>
+            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+                {
+                    categories.map( item => (
+                       <CategoryBox key={item.label} label={item.label} icon={item.icon}></CategoryBox>
+                    ))
+                }
+            </div>
+        </Container>
+    );
+};
+
+export default Categories;
---
+++ src/components/Categories/CategoryBox.jsx
@@ -0,0 +1,11 @@
+
+const CategoryBox = ({label, icon: Icon}) => {
+    return (
+        <div className="flex flex-col items-center justify-center gap-2 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500">
+            <Icon size={20}></Icon>
+            <div className="text-sm font-medium">{label}</div>
+        </div>
+    );
+};
+
+export default CategoryBox;  
