482e36e928db8d5680f9f1fbec5dd965e26fdbd0
Mainul Islam <mainulislam6168@gmail.com>
2024-05-02 08:53:56 +0600

    implement room card and added room to db

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import AuthProvider from './providers/AuthProvider.jsx'
 import { Toaster } from 'react-hot-toast'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes.jsx'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
---
+++ src/pages/Dashboard/AddRoom.jsx
@@ -0,0 +1,80 @@
+import AddRoomForm from "../../components/Forms/AddRoomForm";
+import { useState } from "react";
+import { imageUpload } from "../../apis/imageUpload";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { addRoom } from "../../apis/rooms";
+
+const AddRoom = () => {
+    const { user } = useContext(AuthContext);
+    const [dates, setDates] = useState({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection',
+    })
+    const [loading, setLoading] = useState(false);
+    const [uploadButtonText, setUploadButtonText] = useState('Upload Image')
+    const handleSubmit = event => {
+        event.preventDefault();
+        setLoading(true)
+        const location = event.target.location.value;
+        const title = event.target.title.value;
+        const from = dates.startDate;
+        const to = dates.endDate;
+        const price = event.target.price.value;
+        const total_guest = event.target.total_guest.value;
+        const bedrooms = event.target.bedrooms.value;
+        const bathrooms = event.target.bathrooms.value;
+        const description = event.target.description.value;
+        const category = event.target.category.value;
+        const image = event.target.image.files[0];
+        // upload image
+        imageUpload(image)
+            .then(data => {
+                const roomData = {
+                    location,
+                    image: data.data.display_url, from, to, title, price, total_guest, bedrooms, bathrooms, description,
+                    host: {
+                        name: user?.displayName,
+                        image: user?.photoURL,
+                        email: user?.email,
+                    },
+                    category,
+
+                }
+
+                // post room data to server
+                 addRoom(roomData)
+                 .then(data => console.log(data))
+                 .catch(err => console.log(err))
+
+                setLoading(false)
+            })
+            .catch(err => {
+                console.log(err.message);
+                setLoading(false)
+            })
+
+    }
+
+    const handleImageChange = image => {
+        setUploadButtonText(image.name)
+    }
+    const handleDates = ranges => {
+        setDates(ranges.selection)
+    }
+    return (
+        <div>
+            <AddRoomForm
+                handleSubmit={handleSubmit}
+                loading={loading}
+                handleImageChange={handleImageChange}
+                uploadButtonText={uploadButtonText}
+                dates={dates}
+                handleDates={handleDates}
+            ></AddRoomForm>
+        </div>
+    );
+};
+
+export default AddRoom;
